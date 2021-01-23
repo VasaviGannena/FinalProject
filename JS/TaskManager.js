8b12e37fecf41037c4856d20ca33f7d93fa6538b
vasavi <vasavi5431@gmail.com>
2021-01-24 09:46:36 +1100

    Add task form up-to-date

--- JS/TaskManager.js
+++ JS/TaskManager.js
@@ -1,56 +1,104 @@
-
-  
-class TaskManager {
+  class TaskManager {
     constructor(currentId = 0) {
         this.task = [];
         this.currentId = currentId;
-    }
+      }
+
 
     addTask(formname, formAssignedTo, formduedate, formstatus, formdescription) {
         const newTask = {
-            id: this.curentId++,
+            id: this.currentId++,  
             formname: formname,
             formAssignedTo: formAssignedTo,
             formduedate: formduedate,
-          formdescription: formdescription,
- 
             formstatus: formstatus,
+            formdescription: formdescription   
         };
-
-// console.log(newTask);
-
         this.task.push(newTask);
-    }
-    
+    };
 
-      //update Task//
-      getTaskById(TaskId){
+  
+    deleteTask(taskId) {
+      
+      const newTasks = [];
+          for (let i = 0; i < this.task.length; i++) {
+          const task = this.task[i];
+          if (task.id !== taskId) {
+          newTasks.push(task);
+          }
+            
+      }
+    
+      this.task = newTasks;
+      console.log(this.task);
+  }
+  /* Update status*/ 
+  // Method to get the task id to update status
+  getTaskById(taskId) {
     let foundTask;
-    for(let i=0; i < this.task.length; i++ ){
-      const tasks = this.task[i];
-      if(tasks.id === TaskId ){
+
+    for (let i = 0; i < this.task.length; i++) {
+      
+        const task = this.task[i];
+
+      if(task.id === taskId){
         foundTask = task;
       };
+    };
+    return foundTask;
+        };
 
-      };
-      return foundTask;
-    }
+    save() {
+        // Create a JSON string of the tasks
+      
+      const taskJson = JSON.stringify(this.task);
+  console.log(taskJson);
+      // Store the JSON string in localStorage
+      localStorage.setItem('task', taskJson);
 
+      // Convert the currentId to a string;
+      const currentId = String(this.currentId);
 
+      // Store the currentId in localStorage
+      localStorage.setItem('currentId', currentId);
+        
+      
+        }
 
-   /*Display list of card*/
+      load() {
+          // Check if any tasks are saved in localStorage
+          if (localStorage.getItem('task')) {
+              // Get the JSON string of tasks in localStorage
+              const taskJson = localStorage.getItem('task');
+      console.log(taskJson);
+              // Convert it to an array and store it in our TaskManager
+              this.task = JSON.parse(taskJson);
+              console.log(this.task);
+          }
+      
+          // Check if the currentId is saved in localStorage
+          if (localStorage.getItem('currentId')) {
+              // Get the currentId string in localStorage
+              const currentId = localStorage.getItem('currentId');
+      
+              // Convert the currentId to a number and store it in our TaskManager
+              this.currentId = Number(currentId);
+          }
+      }
+    
+
+  /*Display list of tasks*/
   render() {
       const taskHtmlList = [];
 
       for(let i = 0; i < this.task.length; i++) {
         const tasks = this.task[i];
-    /*Format date - dd/mm/yyyy */
-      // Get the Javascript object new Date, give it the argument tasks.formduedate, and assign it to a variable
+
         const DueDate = new Date(tasks.formduedate);
       // Save the formatted date in a variable
         const formattedDate = DueDate.getDate() + '/' + (DueDate.getMonth() + 1) + '/' + DueDate.getFullYear();  
         
-        const taskHtml = createTaskHtml(tasks.formname, tasks.formAssignedTo, tasks.formdescription, formattedDate, tasks.formstatus);
+        const taskHtml = createTaskHtml(tasks.id, tasks.formname, tasks.formAssignedTo, formattedDate, tasks.formstatus, tasks.formdescription);
 
         taskHtmlList.push(taskHtml);
       };
@@ -58,23 +106,32 @@ class TaskManager {
 
         const taskList = document.querySelector('#task-card');
         taskList.innerHTML = tasksHtml;
+    };
+        
 
-    };  
-    
-}
-const createTaskHtml = ( formname, formdescription, formAssignedTo, DueDate, formstatus) => `
-    <li class="list-group-item" data-task-id=${id}>
-        <div class="d-flex w-100 mt-2 justify-content-between align-items-center">
+  };
+
+  const createTaskHtml = (id, formname, formAssignedTo, formduedate, formstatus, formdescription) => {
+
+    return `
+            <li data-task-id=${id} class="list-group-item mt-2">
+            <div class="d-flex w-100 mt-2 justify-content-between align-items-center">
             <h5>${formname}</h5>
-            <span class="badge ${formstatus === 'TODO' ? 'badge-danger' : 'badge-success'}">${formstatus}</span>
-        </div>
-        <div class="d-flex w-100 mb-3 justify-content-between">
-            <small>Assigned To: ${formAssignedTo}</small>
-            <small>Due: ${DueDate}</small>
-        </div>
-        <p>${formdescription}</p>
-        <div class="d-flex w-100 justify-content-end">
-            <button class="btn btn-outline-success done-button ${formstatus === 'TODO' ? 'visible' : 'invisible'}">Mark As Done</button>
-        </div>
-    </li>
-`;
+            <h5><span style="pull:right;" class="badge ${formstatus === 'To do' || formstatus === 'In progress' || formstatus === 'Review' ? 'badge-warning' : 'badge-success'}">${formstatus}</span></div></h5>
+            <h6><span class="badge ${formAssignedTo === 'Vasavi' ? 'badge-dark' : 'badge-info'}">${formAssignedTo}</span></h6>
+            <div class="d-flex w-100 justify-content-between">
+            <medium><strong>Description:</strong> ${formdescription} </medium>
+            </div>
+            <div class="d-flex w-100 justify-content-between align-items-center">
+            <medium><strong>Due Date:</strong> ${formduedate} </medium>
+            </div>
+            <div>
+            <button class="btn btn-outline-success done-button float-right ${formstatus === 'To do' || formstatus === 'In progress' || formstatus === 'Review' ? 'visible' : 'invisible'}">Mark As Done</button>
+            <button class="btn delete-button btn-outline-dark text-right">Delete</button>
+            </div>
+            </li>
+            `;
+      
+  }
+
+
--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,9 @@
-
+// declare a varable and class 
 const taskManager = new TaskManager(0);
+
+taskManager.load();
+taskManager.render();
 const addTaskForm = document.querySelector('#addTaskForm');
-console.log(taskManager);
 
 
 addTaskForm.addEventListener('submit', (event) => {
@@ -16,65 +18,91 @@ addTaskForm.addEventListener('submit', (event) => {
     const status = document.querySelector('#status');
     const errorMessage = document.querySelector('#alertMessage');
     
-  
-  
+    
     const formname = name.value;
     const formAssignedTo = AssignedTo.value;
     const formduedate = duedate.value;
     const formstatus = status.value;
     const formdescription = description.value;
+    
+// Form validations
+// Form : Name : Validation
     if(!validFormFieldInput(formname)){
         errorMessage.innerHTML = "Need to add a task name";
         errorMessage.style.display = "block"
-          
-    }  
+    
+    } 
+    // Form : AssignedTo  : Validation
     else if(!validFormFieldInput(formAssignedTo)){
         errorMessage.innerHTML = "Need to assign someone";
         errorMessage.style.display = "block"
-   
+
     } 
+    // Form DueDate: Validation
     else if(!validFormFieldInput(formduedate)){
         errorMessage.innerHTML = "Need to add a date";
         errorMessage.style.display = "block"
-   
+
     }
+    // Form : status : validation
     else if(!validFormFieldInput(formstatus)){
         errorMessage.innerHTML = "Need to add a status";
         errorMessage.style.display = "block"
 
     }
+    // Form : Description : validation
     else if(!validFormFieldInput(formdescription)){
         errorMessage.innerHTML = "Need to add a task description";
         errorMessage.style.display = "block"
 
     }
+    // Form : alert message for Error : validation 
     else {
     errorMessage.style.display = "none";
     taskManager.addTask(formname, formAssignedTo, formduedate, formstatus, formdescription);
+    //reset or clear form
     event.target.reset();
-  }
-   
-
-  
-  // Render the tasks
-  taskManager.render();
+}
+    taskManager.save();
+   // Render the tasks
+    taskManager.render();
 });
 
 function validFormFieldInput(data){
-  return data !== null && data !== '';
+    return data !== null && data !== '';
 };
 
-//  update status //
-const TaskList = document.querySelector('#task-card');
 
-TaskList.addEventListener('click', (event) =>{
-    if(event.target.classList.Contains('done-button')) {
+ const taskCard = document.querySelector('#task-card');
+
+    taskCard.addEventListener('click', (event) => {
+    if (event.target.classList.contains('done-button')) {
+        
+        const button = event.target;
+        const parentTask = button.parentElement.parentElement;
+        const taskId = Number(parentTask.dataset.taskId);
+        
+        const task = taskManager.getTaskById(taskId);
+        task.formstatus = 'DONE';
+
+        taskManager.save();
+        taskManager.render();
+    }
+    
+    if (event.target.classList.contains('delete-button')) {
+        // Get the parent Task
+        
         const parentTask = event.target.parentElement.parentElement;
-        const TaskId = Number(parentTask.id);
-        const Task = taskManager.getTaskById(TaskId);
-        Task.status = 'Done';
+        console.log(parentTask);
+        // Get the taskId of the parent Task.
+        const taskId = Number(parentTask.dataset.taskId);
+        console.log(taskId);
+        // Delete the task
+        taskManager.deleteTask(taskId);
+        console.log(taskId);
+        // Save the tasks to localStorage
+        taskManager.save();
+        // Render the tasks
         taskManager.render();
     }
 });
-
-
